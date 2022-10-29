b232b5f3b8ae0d365d513d6d3a2a331dbc9a0726
Arthur Fernandes <arthurhmf@gmail.com>
2022-10-29 14:57:03 -0300

    feat: add new relic

--- android/app/src/main/java/com/tutufernandes/ebisu/MainActivity.java
+++ android/app/src/main/java/com/tutufernandes/ebisu/MainActivity.java
@@ -1,6 +1,15 @@
 package com.tutufernandes.ebisu;
 
+import android.os.Bundle;
+
+import com.newrelic.agent.android.NewRelic;
+
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        NewRelic.withApplicationToken("AA7ae7e54106b9b036d6effc0ec5a6a45956c59d32-NRMA")
+                .start(this.getApplication());
+        super.onCreate(savedInstanceState);
+    }
 }
